b7797c57924318b537f8f5994e623aeb4e3a5fbd
jarvicf <jarvicf@gmail.com>
2023-10-18 20:51:34 -0500

    Clase 18/10/2023

--- ModeloAerolinia_start_oracle.sh
+++ ModeloAerolinia_start_oracle.sh
@@ -1,1 +1,6 @@
-docker run --name oracleAerolinia -p 1521:1521 -e ORACLE_PASSWORD=oracle -e APP_USER=aerolinia -e APP_USER_PASSWORD=oracle -d gvenzl/oracle-xe
+docker run --name oracleAerolinia \
+		-p 1521:1521 \
+		-e ORACLE_PASSWORD=oracle \
+		-e APP_USER=aerolinia \
+		-e APP_USER_PASSWORD=oracle \
+		-d gvenzl/oracle-xe

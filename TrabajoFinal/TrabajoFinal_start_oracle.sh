c049d8d4b863eb2ec4fa47d96ec80b843f595f91
jarvicf <jarvicf@gmail.com>
2023-11-09 22:50:39 -0500

    Desarrollo trabajo final

---
+++ TrabajoFinal/TrabajoFinal_start_oracle.sh
@@ -0,0 +1,4 @@
+docker run --name oracle -p 1521:1521 -e ORACLE_PASSWORD=oracle -e APP_USER=app -e APP_USER_PASSWORD=oracle -e ORACLE_DATABASE=ORCL -v ~/Ucc/Ucc_database_design/Scripts/TrabajoFinal/Oracle:/container-entrypoint-initdb.d -d gvenzl/oracle-xe
+
+
+
